45114262c9471e8facb618607333818e6f532202
n <n>
2019-10-17 15:19:27 -0400

    update

---
+++ animation/sketch.js
@@ -0,0 +1,18 @@
+// meme draft//
+
+var img;
+
+function preload() {
+    img = loadImage("baby.jpeg");
+}
+
+function setup(){
+    createCanvas(640, 480);
+}
+
+function draw(){
+    
+    image(img,0,0, width, height);
+    
+}
+
